5a91a759a3d301bc00fefb92a1a53dd8a782ea9a
jonarroh <jonarrodi99@gmail.com>
2024-06-24 13:32:37 -0600

    test: added test endpoint

--- controller/searchcontroller.py
+++ controller/searchcontroller.py
@@ -62,88 +62,89 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
+import pickle
+import os
+from flask import Flask, request, jsonify
 
-
+app = Flask(__name__)
 
 def getExperienceData(driver):
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        sections = soup.find_all('div', id='experience')
-        if not sections:
-            return None
-        #secctions next sibling
-        next_sibling = sections[0].find_next_sibling()
-        #second next sibling
-        second_next_sibling = next_sibling.find_next_sibling()
-        experiencias = []
-        sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
-        for section in sections:
-            duracion = section.find('span', class_='t-14 t-normal t-black--light').get_text(strip=True)
-            empresa = section.find('span', class_='t-14 t-normal').get_text(strip=True)
-            cargo = section.find('div', class_='display-flex align-items-center mr1 t-bold').get_text(strip=True)
-            actividades = section.find('div', class_='FKFqOBZeKsYhiuZKTYcfkNJIpRwfptzrAEOM').get_text(strip=True)
-            experiencias.append({
-                'duración': duracion,
-                'empresa': empresa,
-                'cargo': cargo,
-                'actividades': actividades
-            })
-        return experiencias
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    sections = soup.find_all('div', id='experience')
+    if not sections:
+        return None
+    # Sections next sibling
+    next_sibling = sections[0].find_next_sibling()
+    # Second next sibling
+    second_next_sibling = next_sibling.find_next_sibling()
+    experiencias = []
+    sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
+    for section in sections:
+        duracion = section.find('span', class_='t-14 t-normal t-black--light').get_text(strip=True)
+        empresa = section.find('span', class_='t-14 t-normal').get_text(strip=True)
+        cargo = section.find('div', class_='display-flex align-items-center mr1 t-bold').get_text(strip=True)
+        actividades = section.find('div', class_='FKFqOBZeKsYhiuZKTYcfkNJIpRwfptzrAEOM').get_text(strip=True)
+        experiencias.append({
+            'duración': duracion,
+            'empresa': empresa,
+            'cargo': cargo,
+            'actividades': actividades
+        })
+    return experiencias
 
 
 def getEducationData(driver):
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        sections = soup.find_all('div', id='education')
-        if not sections:
-            return None
-        #secctions next sibling
-        next_sibling = sections[0].find_next_sibling()
-        #second next sibling
-        second_next_sibling = next_sibling.find_next_sibling()
-        educaciones = []
-        sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
-        for section in sections:
-            duracion = section.find('span', class_='pvs-entity__caption-wrapper').get_text(strip=True) if section.find('span', class_='pvs-entity__caption-wrapper') else "No hay duración"
-            carrera = section.find('span', class_='t-14 t-normal').get_text(strip=True) if section.find('span', class_='t-14 t-normal') else "No hay escuela"
-            escuela = section.find('div', class_='display-flex align-items-center mr1 t-bold').text.strip() if section.find('div', class_='display-flex align-items-center mr1 t-bold') else "No hay carrera"
-            educaciones.append({
-                'duración': duracion,
-                'escuela': escuela,
-                'carrera': carrera
-            })
-        return educaciones
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    sections = soup.find_all('div', id='education')
+    if not sections:
+        return None
+    # Sections next sibling
+    next_sibling = sections[0].find_next_sibling()
+    # Second next sibling
+    second_next_sibling = next_sibling.find_next_sibling()
+    educaciones = []
+    sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
+    for section in sections:
+        duracion = section.find('span', class_='pvs-entity__caption-wrapper').get_text(strip=True) if section.find('span', class_='pvs-entity__caption-wrapper') else "No hay duración"
+        carrera = section.find('span', class_='t-14 t-normal').get_text(strip=True) if section.find('span', class_='t-14 t-normal') else "No hay escuela"
+        escuela = section.find('div', class_='display-flex align-items-center mr1 t-bold').text.strip() if section.find('div', class_='display-flex align-items-center mr1 t-bold') else "No hay carrera"
+        educaciones.append({
+            'duración': duracion,
+            'escuela': escuela,
+            'carrera': carrera
+        })
+    return educaciones
 
 
 def getLicencias_certificacionesData(driver):
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        sections = soup.find_all('div', id='licenses_and_certifications')
-        if not sections:
-            return None
-        #secctions next sibling
-        next_sibling = sections[0].find_next_sibling()
-        #second next sibling
-        second_next_sibling = next_sibling.find_next_sibling()
-        licencias_certificaciones = []
-        sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
-        for section in sections:
-            img = section.find('img')['src'] if section.find('img') else "No hay imagen"
-            nombre = section.find('div', class_='display-flex align-items-center mr1 hoverable-link-text t-bold').get_text(strip=True) if section.find('div', class_='display-flex align-items-center mr1 hoverable-link-text t-bold') else "No hay nombre"
-            plataforma = section.find('span', class_='t-14 t-normal').get_text(strip=True) if section.find('span', class_='t-14 t-normal') else "No hay plataforma"
-            expedicion = section.find('span', class_='t-14 t-normal t-black--light').get_text(strip=True) if section.find('span', class_='t-14 t-normal t-black--light') else "No hay expedicion"
-            URL_certificacion = section.find('a')['href'] if section.find('a') else "No hay URL"
-            licencias_certificaciones.append({
-                'img': img,
-                'nombre': nombre,
-                'plataforma': plataforma,
-                'expedicion': expedicion,
-                'URL_certificacion': URL_certificacion
-            })
-        return licencias_certificaciones
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    sections = soup.find_all('div', id='licenses_and_certifications')
+    if not sections:
+        return None
+    # Sections next sibling
+    next_sibling = sections[0].find_next_sibling()
+    # Second next sibling
+    second_next_sibling = next_sibling.find_next_sibling()
+    licencias_certificaciones = []
+    sections = second_next_sibling.find_all('li', class_='artdeco-list__item')
+    for section in sections:
+        img = section.find('img')['src'] if section.find('img') else "No hay imagen"
+        nombre = section.find('div', class_='display-flex align-items-center mr1 hoverable-link-text t-bold').get_text(strip=True) if section.find('div', class_='display-flex align-items-center mr1 hoverable-link-text t-bold') else "No hay nombre"
+        plataforma = section.find('span', class_='t-14 t-normal').get_text(strip=True) if section.find('span', class_='t-14 t-normal') else "No hay plataforma"
+        expedicion = section.find('span', class_='t-14 t-normal t-black--light').get_text(strip=True) if section.find('span', class_='t-14 t-normal t-black--light') else "No hay expedicion"
+        URL_certificacion = section.find('a')['href'] if section.find('a') else "No hay URL"
+        licencias_certificaciones.append({
+            'img': img,
+            'nombre': nombre,
+            'plataforma': plataforma,
+            'expedicion': expedicion,
+            'URL_certificacion': URL_certificacion
+        })
+    return licencias_certificaciones
 
 
-@search.route('/searcht', methods=['POST'])
+@app.route('/searcht', methods=['POST'])
 def test():
-        
-
     # Configuración de opciones para el navegador
     options = Options()
     options.add_argument('--headless')
@@ -169,42 +170,41 @@ def test():
 
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-    # Navegar al perfil de Maura Ledezma
-    driver.get("https://www.linkedin.com/in/daniel-ruiz-guti%C3%A9rrez-3a925b65/")
-    cookies = driver.get_cookies()
-    for cookie in cookies:
-        print(f"{cookie['name']}: {cookie['value']}")
-    time.sleep(2)
+    if not os.path.exists("cookies.pkl"):
+        # Guardar las cookies en un archivo
+        pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
 
+    cookie = pickle.load(open("cookies.pkl", "rb"))
+    for c in cookie:
+        driver.add_cookie(c)
+
+    # Navegar al perfil de Daniel Ruiz
+    driver.get("https://www.linkedin.com/in/daniel-ruiz-guti%C3%A9rrez-3a925b65/")
+    # Volver a cargar las cookies
+    for c in cookie:
+        driver.add_cookie(c)
+
+    # Esperar a que la página se cargue completamente
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//h1')))
+
+    # Guardar el source de la página en un archivo HTML
     source = BeautifulSoup(driver.page_source, 'lxml')
-    # mandar el source a un archivo html
     with open('source.html', 'w') as file:
         file.write(str(source))
-        
 
-
-
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[1]/div[1]/span[1]/a/h1')))
-
-    # Encontrar el nombre de la persona usando XPath
-    nombre = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[1]/div[1]/span[1]/a/h1')
-    print(nombre.text)
-
-    source = BeautifulSoup(driver.page_source, 'lxml')
+    cookies = driver.get_cookies()
 
     certificaciones = getLicencias_certificacionesData(driver)
 
-    
     driver.quit()
 
-    return {
+    return jsonify({
         "status": "ok",
         "data": {
             "certificaciones": certificaciones,
             "cookies": cookies
         }
-    }
-
+    })
 
 
 
